0b6b7a433315fff0ec696f7b81c009fdf4f66f62
JuanmaJJR <juan.jusue@live.u-tad.com>
2017-03-14 12:19:31 +0100

    primercommit

---
+++ Actividad1-JM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Actividad1-JM
+//
+//  Created by JUAN MARÍA JUSUE ROYAN on 14/3/17.
+//  Copyright © 2017 JUAN MARÍA JUSUE ROYAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
